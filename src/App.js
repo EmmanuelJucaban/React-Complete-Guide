a69af467c49e698b5d0cbff25ae3350af2ac3671
Emmanuel <emmanueljucaban@gmail.com>
2017-11-24 21:49:28 -0800

    Delete logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>This is a React app!</h1>
+        <p>This is really working</p>
+        <Person name={'Oscar'} age='26'/>
+        <Person name={'Frank'} age='39'>
+          My Hobbies: racing
+        </Person>
+        <Person name={'Shane'} age='27'/>
       </div>
     );
   }
