e6599807e9af741ea2e2e7aff50693e5cde48835
Emmanuel <emmanueljucaban@gmail.com>
2017-12-10 14:34:11 -0800

    Component updating life cycle in action

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -14,6 +14,28 @@ class Persons extends Component {
   componentDidMount() {
     console.log("[Persons.js] inside componentDidMount");
   }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("[Update Persons.js] inside componentWillReceiveProps", nextProps);
+  }
+  // this method should return true or false
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Update Persons.js] inside shouldComponentUpdate", nextProps, nextState);
+    // Do not hard code true or false here.
+    // Instead check that based on upcoming props or next state
+    // This only returns true because we used an immutable approach of updating the persons array
+    // This only checks the pointers/ root objects
+    return nextProps.persons !== this.props.persons;
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    console.log("[Update Persons.js] inside componentWillUpdate", nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log("[Update Persons.js] inside componentDidUpdate");
+  }
+
   render() {
     console.log("[Persons.js] inside render");
     return this.props.persons.map((person, index) => {
